7a3f30482570e584a5567bce3c874adca0a0af76
sogomi <sogomib@gmail.com>
2020-02-28 15:02:08 +0300

    Add contact us for customers to communicate with us

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(".card").hover(function(){
     $(this).children(".two").fadeToggle("fast", "linear");
 });
+$(document).ready(function(sogomi){
+    $("button").click(function(){
+        var client=document.getElementById("name").value;
+        alert(client + " " + "Thank you, for contacting us.")
+        sogomi.preventDefault
+    })
+})
