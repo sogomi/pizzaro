da0bc86d11c52c256e1c5043110a8bcc16feca72
sogomi <sogomib@gmail.com>
2020-03-03 07:35:53 +0300

    Add nav bar for easy manupilation

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,6 @@ $(".card").hover(function(){
     $(this).children(".two").show("fast", "linear");
 });
 $(document).ready(function(sogomi){
-    $("button#gervi").click(function(){
-        var client=document.getElementById("name").value;
-        var address=document.getElementById("address").value;
-        alert(client + " " + "Thank you, your order will be delivered @ " + address)
-        sogomi.preventDefault
-    })
-})
-$(document).ready(function(sogomi){
     $("button#javi").click(function(){
         var client=document.getElementById("name").value;
         alert(client + " " + "Thank you, for contacting us")
